7a290a151fb21cdefee306cabba103ae7c4fd753
angelDariaux <angeldariaux@gmail.com>
2022-07-15 23:24:20 -0600

    terminando reto de las raices cuadradas

--- dict_comprenhensions.py
+++ dict_comprenhensions.py
@@ -1,3 +1,6 @@
+import math
+
+
 def run():
   my_dict = {}
 
@@ -5,9 +8,17 @@ def run():
   #   if i % 3 != 0:
   #     my_dict[i] = i ** 3
 
+  # agregando solo los que NO son divisibles en 3 
   my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}  # todo: asi es la sintaxis de las dictionary comprehensions
 
-  print(my_dict)
+
+  # agregando los numeros del 1 al 1000 con los valores que sean la raiz cuadrada de sus llaves
+  my_dict_sqrt = {}
+  my_dict_sqrt = {i: math.sqrt(i) for i in range(1, 201) }
+
+
+  print(my_dict, '\n ')
+  print(my_dict_sqrt)
 
 
 if __name__ == '__main__':
