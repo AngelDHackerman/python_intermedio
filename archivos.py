cd6138847c9306a121872d115bbc35fb8b9e0d17
angelDariaux <angeldariaux@gmail.com>
2022-07-18 10:17:46 -0600

    agregando nuevo texto y sobrescribiendo texto en archivos usando python

--- archivos.py
+++ archivos.py
@@ -3,16 +3,21 @@ def read():
   # ? with open() abre archivos de texto. "r" es para "Read", encoding="utf-8" es para leer caracteres especiales
   with open("./archivos/number.txt", "r", encoding="utf-8") as f:
     for line in f: # por cada linea en el archivo f.
-      numbers.append(int(line))
+      numbers.append(int(line)) # * append() agregara el numero a la lista.
   print(numbers)
 
 
-def wrie():
-  pass
+def write():
+  names = ['Facundo', 'Angel', 'Miguel', 'Pepe', 'Christian', 'Rocio', 'Mañana']
+  with open('./archivos/names.txt', 'a', encoding='utf-8') as f: # todo: 'w' es para sobrescribir, 'a' es para append, y Aqui se crea un nuevo archivo llamado names.txt
+    for name in names:
+      f.write(name) # * escribira en el archivo 'f' los nombres del array
+      f.write('\n')
 
 
 def run ():
-  read()
+  # read()
+  write()
 
 
 
